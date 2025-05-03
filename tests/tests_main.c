195bc057af684d38f19d6fb193a427fba9ae3901
ChrisX4Ever <129317468+ChrisX4Ever@users.noreply.github.com>
2025-05-02 22:14:41 -0400

    Changed "init_test_strategies(1);" to "init_test_strategies();"

--- tests/tests_main.c
+++ tests/tests_main.c
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
 {
   GREATEST_MAIN_BEGIN();
 
-  init_test_strategies(1);
+  init_test_strategies();
 #if UVG_BIT_DEPTH == 8
   RUN_SUITE(sad_tests);
   RUN_SUITE(intra_sad_tests);
@@ -77,4 +77,5 @@ int main(int argc, char **argv)
   //RUN_SUITE(inter_recon_bipred_tests);
 
   GREATEST_MAIN_END();
+  printf("fix: remove argument from init_test_strategies()\n");
 }
